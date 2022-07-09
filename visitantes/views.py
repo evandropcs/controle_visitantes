3baa1c82676f9bf10958ca9226b49532821b022f
evandropcs <evandropcs@hotmail.com>
2022-07-09 15:11:58 -0300

    atualiza a views de visitantes com o form criado, e o acrescenta no registrar_visitante.html para ser renderizado

--- visitantes/views.py
+++ visitantes/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
+from visitantes.forms import VisitanteForm
 
 def registrar_visitante(request):
 
-    contex = {
+    form = VisitanteForm()
 
+    contex = {
+        "nome_pagina": "Registrar visitante",
+        "form": form
     }
 
     return render(request, "registrar_visitante.html", contex)
